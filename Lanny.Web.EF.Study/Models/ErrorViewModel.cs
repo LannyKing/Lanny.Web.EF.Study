d5c78578c27a95240a42a11a98f2eee3fbd76926
LannyKing <30145@qq.com>
2019-07-23 21:24:24 +0800

    添加项目文件。

---
+++ Lanny.Web.EF.Study/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lanny.Web.EF.Study.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
